1c75d5f97958acc90823f548696a561ccae7e9a0
Antishev Yuri <ant_ustas@mail.ru>
2019-08-10 08:46:58 +0300

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path =  require('path');
 
 require('dotenv').config();
 
@@ -23,6 +24,17 @@ const urlRedirect = require('./routes/redirect');
 
 app.use('/urls', urlRouter);
 app.use('/', urlRedirect);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+};
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
